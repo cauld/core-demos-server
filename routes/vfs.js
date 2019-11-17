05c9d2173a65823d1f2d2f728eee06bb1a705599
Chad Auld <cauld@alteryx.com>
2019-11-17 12:48:31 -0700

    Payload updates

--- routes/vfs.js
+++ routes/vfs.js
@@ -12,6 +12,7 @@ router.get('/files', (req, res) => {
   const { rootDirectoryPath, startRow, endRow } = req.query;
   const groupKeys = req.query.groupKeys && Array.isArray(req.query.groupKeys) ? req.query.groupKeys : [];
 
+  // Use the req params to locate the correct pre-saved VFS response
   let fileName = rootDirectoryPath.split('/').join('_').slice(1);
   fileName += groupKeys.length > 0  ? `_${groupKeys.join('_')}` : '';
   fileName += `_${startRow}_${endRow}.json`;
@@ -21,7 +22,7 @@ router.get('/files', (req, res) => {
     if (err) {
       res.status(500).send(err);
     }
-    res.json({ obj });
+    res.json({ ...obj });
   });
 });
 
