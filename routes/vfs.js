ef2f06b321953e0c25bc8359f881fbc7e84020ca
Chad Auld <cauld@alteryx.com>
2019-11-17 11:50:58 -0700

    Payload updates

--- routes/vfs.js
+++ routes/vfs.js
@@ -2,13 +2,27 @@
 This is an example file provider proxy for AEP VFS
 */
 const router = require('express').Router();
-const { sampleVFSResponse } = require('../file-explorer/vfsDataGenerator');
+const jsonfile = require('jsonfile');
 
 // ROUTES
 
 // Route = /api/vfs/files
 router.get('/files', (req, res) => {
-  res.json(sampleVFSResponse);
+
+  /*
+  rootDirectoryPath=%2Fusers%2Ffry
+  startRow=0
+  endRow=100
+
+  const file = '/tmp/data.json'
+  jsonfile.readFile(file, function (err, obj) {
+    if (err) console.error(err)
+    console.dir(obj)
+  })
+  */
+
+  //res.json(sampleVFSResponse);
+  res.send('FOO');
 });
 
 module.exports = router;
