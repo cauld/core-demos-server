897ff063e0bcdeb83dd5937fd99b1554e007cc74
Chad Auld <cauld@alteryx.com>
2019-11-18 12:43:47 -0700

    Fixing groupKeys

--- routes/vfs.js
+++ routes/vfs.js
@@ -3,6 +3,7 @@ This is an example file provider proxy for AEP VFS
 */
 const router = require('express').Router();
 const jsonfile = require('jsonfile');
+const fs = require('fs');
 const path = require('path');
 
 // ROUTES
@@ -10,7 +11,7 @@ const path = require('path');
 // Route = /api/vfs/files
 router.get('/files', (req, res) => {
   const { rootDirectoryPath, startRow, endRow } = req.query;
-  const groupKeys = req.query.groupKeys && Array.isArray(req.query.groupKeys) ? req.query.groupKeys : [];
+  const groupKeys = req.query.groupKeys ? req.query.groupKeys.split(',') : [];
 
   // Use the req params to locate the correct pre-saved VFS response
   let fileName = rootDirectoryPath.split('/').join('_').slice(1);
@@ -18,12 +19,16 @@ router.get('/files', (req, res) => {
   fileName += `_${startRow}_${endRow}.json`;
   const filePath = path.resolve(__dirname, '../file-explorer/data/vfs/', fileName);
 
-  jsonfile.readFile(filePath, (err, obj) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.json({ ...obj });
-  });
+  if (fs.existsSync(filePath)) {
+    jsonfile.readFile(filePath, (err, obj) => {
+      if (err) {
+        res.status(500).send('Cannot locate data!');
+      }
+      res.json({ ...obj });
+    });
+  } else {
+    res.status(500).send('Cannot locate data!');
+  }
 });
 
 module.exports = router;
